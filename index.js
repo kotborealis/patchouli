08fa6bc2e55c7ce1403ecbfa1a7b143e8558d43c
kotborealis <kotborealis@gmail.com>
2017-09-11 00:37:08 +0300

    working!! :apple:

--- index.js
+++ index.js
@@ -10,54 +10,60 @@ const glob = require('glob');
 const gaze = require('gaze');
 const outputFilename = require('./lib/outputFilename');
 const fs = require('fs');
+const md_only = require('./lib/md_only_filter')
 
-const file_extension_is = (filename, ext) => path.extname(filename) === '.' + ext;
-const ignore_ext_filter = (file) =>
-    !config.ignore_ext.reduce((bool, ext) => file_extension_is(file, ext) || bool, false);
-
-const build_file = (input) => {
-    if(!ignore_ext_filter(input)) return;
-
-    console.log("Building file ", input);
-    const file = path.isAbsolute(input) ? input : path.join(process.cwd(), input);
-    const type = args.type || args.t || 'html';
-    const output = args.output || args.o || outputFilename(file, type);
-    build(file, output, type);
-};
-
-const build_glob = (patterns) =>
-    patterns
-        .forEach(pattern =>
-            glob.sync(pattern)
-                .filter(ignore_ext_filter)
-                .forEach(build_file)
-        );
-
-const watch_glob = (patterns) => {
-    patterns.forEach(pattern => {
-        gaze(pattern, function(){
+// glob.sync(pattern)
+/*
+gaze(pattern, function(){
             this.on('changed', build_file);
             this.on('added', build_file);
         });
-    });
+ */
+
+const build_file = file => {
+    console.log("Building file ", file);
+    build(file, outputFilename(file, type), type);
 };
 
-const input_files = args._;
+const to_plain_array = (a, b) => [...a, ...b];
 
-if(args.clean || args.c){
-    console.log("Cleaning");
-    config.clean_ext.forEach(ext => glob.sync(`*.${ext}`).forEach(fs.unlinkSync.bind(fs)));
-}
+const markdown_files = args._
+    .map(pattern => glob.sync(pattern))
+    .reduce(to_plain_array, [])
+    .filter(md_only);
 
-if(input_files.length){
-    if(args.w || args.watch){
-        console.log("Watching files");
-        build_glob(input_files);
-        watch_glob(input_files);
+const targets = markdown_files.length ? markdown_files : glob.sync('*.md');
+
+const act_watch = args.w || args.watch || args._.indexOf('watch') >= 0;
+const act_clean = args.c || args.clean || args._.indexOf('clean') >= 0;
+const act_build = !act_watch && !act_clean;
+
+const type = args.type || args.t || 'html';
+
+switch(true){
+    case act_build: {
+        console.log("Building targets...");
+        targets.forEach(build_file);
+        break;
     }
-    else{
-        console.log("Building files");
-        build_glob(input_files);
+    case act_watch: {
+        console.log("Building targets...");
+        targets.forEach(build_file);
+        console.log("Watching targets...");
+        gaze(targets, function(){
+            this.on('changed', file =>
+                setTimeout(() => build_file(file), 200)
+            );
+        });
+        break;
+    }
+    case act_clean: {
+        console.log("Cleaning...");
+        targets
+            .map(target => config.clean_ext.map(ext => outputFilename(target, ext)))
+            .reduce(to_plain_array)
+            .forEach(file => fs.unlink(file, () => null));
+        break;
     }
 }
 
