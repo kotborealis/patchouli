f50b936149b64c9cbeab7f8e9bdc3cc64dfb985b
kotborealis <kotborealis@gmail.com>
2020-05-22 04:17:07 -0400

    removed unused pandoc-install script

--- docker/pandoc-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -kL https://github.com/jgm/pandoc/releases/download/2.4/pandoc-2.4-linux.tar.gz \
-    | tar -xz --strip-components=1 -C /usr/
-
-curl -kL https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.3.0/linux-ghc84-pandoc23.tar.gz \
-    | tar -xz --strip-components=1 -C /usr/bin/
