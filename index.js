fa9a8610f3f79a1742dfa91e13e7257ac97972c7
kotborealis <kotborealis@gmail.com>
2020-12-15 21:47:38 +0300

    refactoring

--- index.js
+++ index.js
@@ -6,29 +6,15 @@ const glob = require('glob');
 const md_only = require('./lib/predicate_md_files');
 const build_document = require('./lib/document_builder');
 const debug = require('./lib/debug').extend('main');
+const {showVersion, showUsage} = require('./lib/help');
 
 if(args.v || args.version) {
-const package_info = require('./package.json');
-    console.log(`
-${package_info.name} \t v.${package_info.version}
-\t* github: \t ${package_info.repository.url}
-\t* author: \t ${package_info.author}
-    `.trim());
+    showVersion();
     return;
 }
+
 if(args.h || args.help){
-    const {dirname, resolve} = require('path');
-    console.log(`
-Patchouli: converts markdown files in working directory to specified type.
-Uses .patchouli.js from working directory as config,
-see ${resolve(dirname(require.resolve('./')), '.patchouli.js')} for reference
-Usage:
-\t-v, --version \t\t View version info.
-\t-h, --help \t\t View this guide.
-\t-t=name, --type=name \t Convert markdown to specified type.
-\t\t\t\t Supports: tex, pdf, revealjs, html, docx
-\t--config=file \t\t Specify full path to configuration file
-    `.trim());
+    showUsage();
     return;
 }
 
