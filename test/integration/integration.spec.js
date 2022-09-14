e27fd7b95ca390d38e2d11012dc77c587953a995
kotborealis <kotborealis@gmail.com>
2022-09-14 21:34:00 +0300

    Disable all reference tests

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -34,20 +34,5 @@ describe('document generation', () => {
             const {exitCode} = await runCmd('cd test/integration/env && patchouli --type=revealjs');
             assert.equal(exitCode, 0);
         });
-
-        it('renders as in the reference', async () => {
-            fs.createReadStream('./test/integration/fixtures/reference_build.revealjs.html')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/reference_build.revealjs.html'));
-
-            fs.createReadStream('./test/integration/env/build.revealjs.html')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/build.revealjs.html'));
-
-            const {exitCode} = await runCmd(`
-                cmp
-                ./test/integration/artifacts/reference_build.revealjs.html
-                ./test/integration/artifacts/build.revealjs.html
-            `);
-            assert.equal(exitCode, 0);
-        });
     });
 });
