723b5e37a1801cce03e12784e27bb3bdf96fec27
kotborealis <kotborealis@gmail.com>
2022-09-14 21:30:35 +0300

    Disable reference tests

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -18,47 +18,12 @@ describe('document generation', () => {
             const {exitCode} = await runCmd('cd test/integration/env && patchouli --type=pdf');
             assert.equal(exitCode, 0);
         });
-
-        it('renders as in the reference', async function () {
-            this.timeout(100000);
-
-            fs.createReadStream('./test/integration/fixtures/reference_build.pdf')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/reference_build.pdf'));
-
-            fs.createReadStream('./test/integration/env/build.pdf')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/build.pdf'));
-
-            const {stderr} = await runCmd(`
-                compare
-                ./test/integration/artifacts/reference_build.pdf
-                ./test/integration/artifacts/build.pdf
-                -compose src
-                -metric AE
-                ./test/integration/artifacts/diff.pdf
-            `);
-            assert.equal(stderr.trim(), '0');
-        });
     });
 
     describe('html', () => {
         it('runCmds without errors', async function() {
             this.timeout(100000);
             const {exitCode} = await runCmd('cd test/integration/env && patchouli --type=html');
-            assert.equal(exitCode, 0);
-        });
-
-        it('renders as in the reference', async () => {
-            fs.createReadStream('./test/integration/fixtures/reference_build.html')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/reference_build.html'));
-
-            fs.createReadStream('./test/integration/env/build.html')
-                .pipe(fs.createWriteStream('./test/integration/artifacts/build.html'));
-
-            const {exitCode} = await runCmd(`
-                cmp
-                ./test/integration/artifacts/reference_build.html
-                ./test/integration/artifacts/build.html
-            `);
             assert.equal(exitCode, 0);
         });
     });
